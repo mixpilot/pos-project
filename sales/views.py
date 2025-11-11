3668d281fd6c937723c8f57b8bbf3aa75419b2da
mixpilot <128009971+mixpilot@users.noreply.github.com>
2025-11-11 23:15:00 +0300

    Login Required

--- sales/views.py
+++ sales/views.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, SaleTransaction, SaleItem
 from django.utils import timezone
-
-#Display all products
+@login_required
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'sales/product_list.html', {'products': products})
 
-# Add a new product
+
+@login_required
 def add_product(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -34,6 +35,8 @@ def add_product(request):
 
     return render(request, 'sales/add_product.html')
 
+
+@login_required
 def record_sale(request):
     products = Product.objects.all()
 
@@ -58,16 +61,20 @@ def record_sale(request):
         return redirect('record-sale')
     return render(request, 'sales/record_sale.html', {'products': products})
 
+
+@login_required
 def sales_summary(request):
     transactions = SaleTransaction.objects.all().order_by('-timestamp')
     return render(request, 'sales/sales_summary.html', {'transactions': transactions})
 
+
+@login_required
 def transaction_detail(request, transaction_id):
     transaction = get_object_or_404(SaleTransaction, id=transaction_id)
     items = SaleItem.objects.filter(transaction=transaction)
     return render(request, 'sales/transction_detail.html', {
         'transaction': transaction,
-        'items':items
+        'items': items
     })
 
 
